62f0f3122df25f7c33d554892655218d6cce1b55
Amos <amosbi12@gmail.com>
2019-03-17 10:40:46 +0200

    Will change pack function params and add buttons

--- GUI.py
+++ GUI.py
@@ -8,15 +8,29 @@ This module is consist of a Graphical User Interface
 """
 
 
-def pack(*args, side=None):
+def pack(*args):
     for item in args:
-        item.pack(side)
+        item.pack()
 
 
 def main():
 
     # Creating a blank window (root var)
     root = Tk()
+
+    # Creating a frame
+    frame = Frame(root)
+
+    # Creating buttons
+    button1 = Button(frame, text="Button1")
+    button2 = Button(frame, text="Button2")
+    button3 = Button(frame, text="Button3")
+    button4 = Button(frame, text="Button4")
+
+
+
+    # Packing the whole items
+    pack(frame, button1, button2, button3, button4)
 
     # Making the window appear constantly
     root.mainloop()
