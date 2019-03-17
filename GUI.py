a565c5fcf5191816b0e4847301217414b11b8e85
Amos <amosbi12@gmail.com>
2019-03-17 10:34:48 +0200

    Will add a blank window

--- GUI.py
+++ GUI.py
@@ -2,8 +2,19 @@ from tkinter import *
 from classes import Server, Client
 
 
+"""
+This module is consist of a Graphical User Interface
+(GUI) and uses the classes module I have built.
+"""
+
+
 def main():
-    pass
+
+    # Creating a blank window (root var)
+    root = Tk()
+
+    # Making the window appear constantly
+    root.mainloop()
 
 
 if __name__ == '__main__':
